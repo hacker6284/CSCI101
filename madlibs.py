710ff3aa69534946d4b4fbb3fec36e494c78174c
hacker6284 <hacker.6284@gmail.com>
2016-09-08 20:58:50 -0600

    Mad Lib
    Declaration of Inependence Mad Libs

---
+++ madlibs.py
@@ -0,0 +1,31 @@
+n1 = raw_input("Enter a Noun: ")
+a1 = raw_input("Enter an Adjective: ")
+v1 = raw_input("Enter a Verb: ")
+a2 = raw_input("Enter an Adjective: ")
+pn1 = raw_input("Enter a Plural Noun: ")
+pn6 = raw_input("Enter a Plural Noun: ")
+v2 = raw_input("Enter a Verb: ")
+pn2 = raw_input("Enter a Plural Noun: ")
+a3 = raw_input("Enter an Adjective: ")
+a4 = raw_input("Enter an Adjective: ")
+n3 = raw_input("Enter a Noun: ")
+propn1 = raw_input("Enter a Proper Noun: ")
+propn2 = raw_input("Enter a Proper Noun: ")
+a5 = raw_input("Enter an Adjective: ")
+pn3 = raw_input("Enter a Plural Noun: ")
+pn4 = raw_input("Enter a Plural Noun: ")
+v3 = raw_input("Enter a Verb: ")
+pn5 = raw_input("Enter a Plural Noun: ")
+n4 = raw_input("Enter a Noun: ")
+print "When in the Course of " + n1 +" events,"
+print "it becomes " + a1 +" for one people"
+print "to " + v1 + " the " + a2 + " " + pn1 + " which have"
+print "connected them with " + pn6 + ",and to "
+print v2 + " among the " + pn2 + " of the earth, "
+print "the " + a3 + " and " + a4 + " " + n3
+print "to which the Laws of " + propn1 +" and of "
+print propn1 + "'s " + propn2 + " entitle them, "
+print "a " + a5 + " respect to the " + pn3
+print "of " + pn4 + " requires that they should "
+print v3 + " the " + pn5 + " which impel them"
+print "to the " + n4 + "."
