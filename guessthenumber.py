45dfd6c64bfb27f41ec08959c6ebd4d444628794
hacker6284 <hacker.6284@gmail.com>
2016-09-08 20:34:30 -0600

    Guess the Number Game

---
+++ guessthenumber.py
@@ -0,0 +1,23 @@
+import random
+play = raw_input("Play Guess the Number?(y/n)\n")
+while play == "y":
+    n = random.randint(1,100)
+    win = 0
+    while win == 0:
+        g = input("Input Guess Between 1 and 100:")
+        if g < n:
+            print "Higher"
+        elif g > n:
+            print "Lower"
+        elif g > 100:
+            print "Between 1 and 100!!"
+        elif g < 1:
+            print "Between 1 and 100!!"
+        elif g == n:
+            print "You did it!"
+            win = 1
+        else:
+            print "error"
+    play = raw_input("Play Again?(y/n)\n")
+print "Damn"
+    
