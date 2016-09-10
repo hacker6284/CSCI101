8f4606fbf553baf426f8365762256cb5eb59abd9
hacker6284 <hacker.6284@gmail.com>
2016-09-09 19:08:40 -0600

    Updates
    changed input to raw_input and added some more names

--- dicesim.py
+++ dicesim.py
@@ -3,7 +3,7 @@ roll = raw_input("Would you like to roll?(y/n)\n")
 if roll == "y":
     roll = 1
     while roll == 1:
-        s = input("How many sides?\n")
+        s = int(raw_input("How many sides?\n"))
         d = random.randint(1,s)
         print d
         if d == s:
--- guessthenumber.py
+++ guessthenumber.py
@@ -4,7 +4,7 @@ while play == "y":
     n = random.randint(1,100)
     win = 0
     while win == 0:
-        g = input("Input Guess Between 1 and 100:")
+        g = int(raw_input("Input Guess Between 1 and 100:"))
         if g < n:
             print "Higher"
         elif g > n:
