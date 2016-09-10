ec744e6fffc9ef0d68b4881f72513be701a855ef
hacker6284 <hacker.6284@gmail.com>
2016-09-09 22:17:16 -0600

    more math

--- importantmath.py
+++ importantmath.py
@@ -4,3 +4,8 @@ print math.cos(0)
 angle = math.radians(random.randint(0,360))
 print math.cos(angle)
 print math.floor(math.cos(angle))
+for i in range(0,100):
+    x = random.randint(0,1000)
+    y = random.randint(0,1000)
+    print math.fmod(x,y)
+    print x % y
