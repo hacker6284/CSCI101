14eb54b4cbb1e42a194dc5db2af89c85d6ab9a6d
hacker6284 <zachmills@android-d86bf922a982372f.wireless.mines.edu>
2016-11-28 16:13:28 -0700

    Changed some arbitrary base stuff

--- ArbitraryBase.py
+++ ArbitraryBase.py
@@ -1,7 +1,7 @@
 import math
 
 Basestring = raw_input("Enter the coefficient symbols: ")
-x = int(raw_input("Enter a non-negative integer to convert: "))
+x = 1
 
 def Encode(x,string):
 	B = len(string)
@@ -11,7 +11,9 @@ def Encode(x,string):
 	return output + string[(x%B)]
 	#else:
 		#return output
-if (x>=0):
-	print "Base %d: %s"%(len(Basestring),Encode(x,Basestring))
-else:
-	print "Goodbye!"
+while (x>=0):
+	x = int(raw_input("Enter a non-negative integer to convert: "))
+	if (x<0):
+		print "Goodbye!"
+	else:	
+		print "Base %d: %s"%(len(Basestring),Encode(x,Basestring))
