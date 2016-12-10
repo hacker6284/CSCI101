289d024d729de745abf27535ba1503cdfc1fd8dc
hacker6284 <hacker.6284@gmail.com>
2016-12-10 13:11:51 -0700

    tryna fix shit

--- bubbleexcel.py
+++ bubbleexcel.py
@@ -1,3 +1,5 @@
+import random
+import time
 from openpyxl import Workbook
 from openpyxl import load_workbook
 wb = load_workbook('list.xlsx')
@@ -18,7 +20,11 @@ while j < length -1:
     i = length -1
     while i > 0:
         if contenders[i][0] not in contenders[i-1]:
-            better = int(raw_input("Is %s better than %s? 1 for yes, 0 for no"%(contenders[i][0],contenders[i-1][0])))
+            #better = int(raw_input("Is %s better than %s? 1 for yes, 0 for no"%(contenders[i][0],contenders[i-1][0])))
+            #better = random.randint(0,1)
+            better = 0
+            time.sleep(1)
+            print "Is %s better than %s? 1 for yes, 0 for no"%(contenders[i][0],contenders[i-1][0])
             if better == 0:
                 contenders[i-1] += [contenders[i][0]]
             else:
